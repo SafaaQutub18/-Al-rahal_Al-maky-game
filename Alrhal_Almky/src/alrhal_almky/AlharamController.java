79e8837f6f437e1c5fc3e5ad1862cfe525693d04
nuhaharbi <nuhaharbi98@gmail.com>
2021-09-29 17:52:44 +0300

    add images file inside the project

--- Alrhal_Almky/src/alrhal_almky/AlharamController.java
+++ Alrhal_Almky/src/alrhal_almky/AlharamController.java
@@ -7,26 +7,18 @@ package alrhal_almky;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author Nuha
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class AlharamController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
