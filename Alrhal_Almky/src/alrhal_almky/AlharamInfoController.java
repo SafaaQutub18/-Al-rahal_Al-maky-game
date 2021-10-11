88aa1931364bcd0a7b95fb98f65bddf3174334b4
Arwa <51625399+arwagh@users.noreply.github.com>
2021-10-11 20:54:30 +0300

    Add info scenes

---
+++ Alrhal_Almky/src/alrhal_almky/AlharamInfoController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alrhal_almky;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author argha
+ */
+public class AlharamInfoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
